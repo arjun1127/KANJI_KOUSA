70dd5c5ebab488d2a9dc07a7cbef02b0893798bf
arjunrao <arjunkrao2004@gmail.com>
2025-06-07 08:30:38 +0530

    deploy changes

--- backend/Server.js
+++ backend/Server.js
@@ -13,7 +13,7 @@ const app=express();
 app.use(express.json())
 
 const PORT = process.env.PORT || 5000;
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
 
 // app.get("/",(req,res)=>{
 //     res.send("kanji practice is listning");
@@ -39,6 +39,6 @@ app.use('/api/kanji', kanjiRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/quiz',quizStoreRoutes);
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on  http://localhost:${PORT}`);
-})
+// app.listen(PORT,()=>{
+//     console.log(`Server is running on  http://localhost:${PORT}`);
+// })
