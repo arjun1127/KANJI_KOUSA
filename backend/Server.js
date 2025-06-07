8e107bad2a45f257a083b523775e7bf480d7f87d
arjunrao <arjunkrao2004@gmail.com>
2025-06-07 09:05:14 +0530

    renderError

--- backend/Server.js
+++ backend/Server.js
@@ -39,6 +39,6 @@ app.use('/api/kanji', kanjiRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/quiz',quizStoreRoutes);
 
-// app.listen(PORT,()=>{
-//     console.log(`Server is running on  http://localhost:${PORT}`);
-// })
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
